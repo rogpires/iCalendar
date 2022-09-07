e7f9fe85aae5dcf7d28fb3cda0b68106a97234a6
Rogerio Pires <rogeriocpires@hotmail.com>
2022-09-06 21:30:56 -0300

    Start
    Start

---
+++ iCalendar/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iCalendar
+//
+//  Created by Rogerio Pires on 06/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ///ui
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCalendar/iCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalendarApp.swift
+//  iCalendar
+//
+//  Created by Rogerio Pires on 06/09/22.
+//
+
+import SwiftUI
+
+@main
+struct iCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
